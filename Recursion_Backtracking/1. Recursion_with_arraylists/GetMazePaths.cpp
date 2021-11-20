c21a0e579e6a0a10f397e2886a0bee7f313b74d1
Anupam <anusrivastava910@gmail.com>
2021-11-20 09:37:59 +0530

    Completed Recursion with arraylists

---
+++ Recursion_Backtracking/1. Recursion_with_arraylists/GetMazePaths.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+// sr - source row
+// sc - source column
+// dr - destination row
+// dc - destination column
+vector <string> getMazePaths(int sr, int sc, int dr, int dc) {
+    //Base cases
+    vector<string> baseCase;
+    if(sr==dr && sc==dc){
+        baseCase.push_back("");
+        return baseCase;
+    } else if(sr>dr || sc>dc) {
+        return baseCase;
+    }
+
+    vector<string> paths1 = getMazePaths(sr, sc+1, dr, dc);
+    vector<string> paths2 = getMazePaths(sr+1, sc, dr, dc);
+
+    vector<string> allPaths;
+    for(string s: paths1) allPaths.push_back("h"+s);
+    for(string s: paths2) allPaths.push_back("v"+s);
+    
+    return allPaths;    
+}
